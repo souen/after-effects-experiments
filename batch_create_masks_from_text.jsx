32517dbdb9dbfefc562bfa5203c8cdd7cc4ac0bf
Souen BONIFACE <souenboniface@gmail.com>
2014-10-01 18:09:53 +0200

    Remove "confirm" msg since user can undo script action

--- batch_create_masks_from_text.jsx
+++ batch_create_masks_from_text.jsx
@@ -23,7 +23,6 @@
 
     app.beginUndoGroup("create masks from text");
 
-    var msg = '';
     var selected_text_layers = []
     var project = app.project;
 
@@ -36,33 +35,29 @@
                 var layer = selected_layers[j];
                 if (layer instanceof TextLayer) {
                     selected_text_layers.push(layer);
-                    msg += '- "' + layer.name + '" (from composition "' + layer.containingComp.name + '")\n';
                 }
             }
         }
     }
 
     if (selected_text_layers.length > 0) {
-        msg = 'Create masks from following layers ?\n' + msg;
-        if (confirm(msg)) {
-            // Un-select all layers in order to have access to righ-click command "Create mask from text"
-            for (i = 0; i < selected_text_layers.length; i++) {
-                var layer = selected_text_layers[i];
-                layer.selected = false;
-            }
-            // Loop again, select layers one by one and call command
-            for (i = 0; i < selected_text_layers.length; i++) {
-                var layer = selected_text_layers[i];
-                layer.selected = true;
-                var createMasksCommand = 2933; // result of app.findMenuCommandId("Créer des masques à partir du texte")
-                                               // but since it is localized, we use the id...
-                                               // FIXME: find a way to use localized labels ?
-                app.executeCommand(createMasksCommand);
-                // The "create masks from text" command create a shape layer (containing the mask)
-                // and *select* it. So we have so deselect all layers to be sure
-                // the right click command is still available.
-                unselectAllLayersInComp(layer.containingComp);
-            }
+        // Un-select all layers in order to have access to righ-click command "Create mask from text"
+        for (i = 0; i < selected_text_layers.length; i++) {
+            var layer = selected_text_layers[i];
+            layer.selected = false;
+        }
+        // Loop again, select layers one by one and call command
+        for (i = 0; i < selected_text_layers.length; i++) {
+            var layer = selected_text_layers[i];
+            layer.selected = true;
+            var createMasksCommand = 2933; // result of app.findMenuCommandId("Créer des masques à partir du texte")
+                                           // but since it is localized, we use the id...
+                                           // FIXME: find a way to use localized labels ?
+            app.executeCommand(createMasksCommand);
+            // The "create masks from text" command create a shape layer (containing the mask)
+            // and *select* it. So we have so deselect all layers to be sure
+            // the right click command is still available.
+            unselectAllLayersInComp(layer.containingComp);
         }
     } else {
         alert('Please select at least one text layer.');
